172357dcb4d7f62ca800d30178093b6e12857475
zshen <jason.lim@digi-x.my>
2021-07-14 22:50:49 +0800

    Remove unnecesary files & created frontend

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,43 @@
-import styles from '../styles/Home.module.css'
+import Head from 'next/head'
 
-export default function Home () {
+const Home = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hello <a href='https://nextjs.org'>Next.js!</a>
-        </h1>
+    <div className="px-8 mt-10">
+      <Head>
+        <title>The White Flag Project</title>
+      </Head>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <div className="mx-auto">
+        <img className="rounded-tr-md rounded-tl-md h-48 w-full" src="/images/pexels-alex.jpg" />
+      </div>
 
-        <div className={styles.grid}>
-          <a href='https://nextjs.org/docs' className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+      <div className="bg-white p-8 rounded-br-md rounded-bl-md mt-10">
+        <h2 className="text-gray-700 font-semibold">Foodbanker Portal</h2>
+        <br/>
+
+
+        <div className="flex flex-col items-stretch  mt-8 ">
+
+          <a href="/scancollector" className="buttons">
+            <img className="buttonsImage" src="/images/qrcustomer.png"/>
+            <h2 className="text-sm text-center text-gray-600 mt-4 ">Scan Collector</h2>
           </a>
 
-          <a href='https://nextjs.org/learn' className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <br/>
+
+          <a href="/" className="buttons">
+            <img className="buttonsImage" src="/images/category.png"/>
+            <h2 className="text-sm text-center text-gray-600 mt-4">Choose Category</h2>
           </a>
 
-          <a
-            href='https://github.com/vercel/next.js/tree/master/examples'
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
-          <a
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
-        </a>
-      </footer>
+      </div>
+
     </div>
   )
 }
+
+
+export default Home
