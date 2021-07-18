31ab5ca5866bc228aaa12cf704b7fd0db004e958
zshen <jason.lim@digi-x.my>
2021-07-18 23:12:32 +0800

    Updated some UI & foodbank manual entry

--- pages/index.js
+++ pages/index.js
@@ -34,17 +34,40 @@ function Home ({ data }) {
 
   return userDetails ? (
     <Layout>
-      <h2>{userDetails.userID}</h2>
-      <QRCode value={userDetails._id} />
+      <div className='bg-white p-8 rounded-br-md rounded-bl-md mt-10 flex flex-col items-strech'>
+        <div className='flex-1'>
+          <h2 className='text-gray-700 font-semibold'>Profile</h2>
+        </div>
+
+        <div className='flex flex-col items-stretch  mt-8 justify-items-center'>
+          <h2>NRIC: {userDetails.userID}</h2>
+          <br/>
+          <QRCode value={userDetails._id} />
+          <br/>
+          <p>Kindly show the QR code to the foodbank</p>
+        </div>
+
+      </div>
     </Layout>
   ) : (
     <Layout>
       <div className='bg-white p-8 rounded-br-md rounded-bl-md mt-10 flex flex-col items-strech'>
-        <form onSubmit={loginUser}>
-          <label htmlFor='idNumber'>ID Number</label>
-          <input id='idNumber' name='idNumber' type='text' autoComplete='name' required />
-          <button type='submit'>Login</button>
-        </form>
+        <div className='flex-1'>
+          <h2 className='text-gray-700 font-semibold'>Login / Register</h2>
+        </div>
+
+        <div className='flex flex-col items-stretch mt-8 justify-items-center'>
+          <form onSubmit={loginUser}>
+            <label htmlFor='idNumber'>Key in your NRIC</label>
+            <br/>
+            <input id='idNumber' name='idNumber' type='text' autoComplete='name' placeholder='NRIC' className="inputField" required />
+            <br/>
+            <button type='submit' className='approveButton'>
+              Login
+            </button>
+          </form>
+        </div>
+
       </div>
     </Layout>
   )
